d203669ba51cc3e646da84af9cb636844890d51b
Imkong <imkongimchen9@gmail.com>
2025-02-17 14:26:42 +0530

    added time table in instudia

---
+++ instudia.py
@@ -0,0 +1,9 @@
+def getMultiply(a,b):
+    return a *b
+
+def getTable(num):
+    for i in range(0,11):
+        result = getMultiply(num, i)
+    print(f'{num} x {i} = {result}')
+    
+getTable(5)
